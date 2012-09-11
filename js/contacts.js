3603e0662326a7e8acdb915a3302ee6b95e17050
hawka <amalia.hawkins@gmail.com>
2012-09-10 21:50:00 -0400

    rendering contacts

--- js/contacts.js
+++ js/contacts.js
@@ -10,13 +10,27 @@ $(document).ready(function() {
         contacts[name] = number;
         localStorage["contacts"] = JSON.stringify(contacts);
     }
+    // shows a single contact
+    function addOne(name, number) {
+    }
+    // show all contacts
+    function render() {
+        var data = localStorage["contacts"];
+        var contacts = JSON.parse(data);
+        for (var name in contacts) {
+            addOne(name, contacts[name]);
+        }
+    }
     $(".contact-form").on("submit", function(e) {
         e.preventDefault();
         var name = $("#name").val();
         var number = $("#number").val();
         save(name, number);
+        addOne(name, number);
         console.log(name + " " + number);
     });
+
     if (!localStorage["contacts"])
         localStorage["contacts"] = "{}";
+    render();
 });
