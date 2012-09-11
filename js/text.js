b87958da0e119572a86e50ed00503cdaaac5295f
hawka <amalia.hawkins@gmail.com>
2012-09-10 22:31:22 -0400

    getting the body

--- js/text.js
+++ js/text.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
     $(".text-form").hide();
+
     $(".text").live("click", function() {
         $(this).closest(".contact").find(".text-form").slideToggle();
+    });
+
+    $(".text-form").live("submit", function(e) {
+        e.preventDefault();
+        var options = {};
+        options["to"] = ""; //number
+        options["body"] = $(this).find(".text-body").val(); //body
+        console.log(options);
+        // send text
+        var url = "";
     });
 });
